620560533f6936a8a9d565d4ea3f66fc3647e6bb
Stan Cheng <stanley.cheng@disney.com>
2021-01-21 17:12:02 -0800

    removing aws_autoscale_group

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -141,19 +141,19 @@ resource "aws_key_pair" "user" {
 #  }
 #}
 
-resource "aws_autoscaling_group" "asg" {
-  name = "${var.name}-asg"
-
-  launch_configuration = "${aws_launch_configuration.instance.name}"
-  vpc_zone_identifier  = ["${var.vpc_subnets}"]
-  max_size             = "${var.asg_max_size}"
-  min_size             = "${var.asg_min_size}"
-  desired_capacity     = "${var.asg_desired_size}"
-
-  health_check_grace_period = 300
-  health_check_type         = "EC2"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#resource "aws_autoscaling_group" "asg" {
+#  name = "${var.name}-asg"
+#
+#  launch_configuration = "${aws_launch_configuration.instance.name}"
+#  vpc_zone_identifier  = ["${var.vpc_subnets}"]
+#  max_size             = "${var.asg_max_size}"
+#  min_size             = "${var.asg_min_size}"
+#  desired_capacity     = "${var.asg_desired_size}"
+#
+#  health_check_grace_period = 300
+#  health_check_type         = "EC2"
+#
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#}
